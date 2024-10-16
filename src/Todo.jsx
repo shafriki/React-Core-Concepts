d7d964a8f7b0aa32436e6d9aca2cc86733cba7fb
shafriki <shafriki.cse@gmail.com>
2024-10-16 06:21:51 +0600

    six type conditional rendering

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,12 +5,28 @@
 // }
 
 // conditional rendering option: 1
-export default function Todo({task, isDone}) {
+// export default function Todo({task, isDone}) {
     
-        if(isDone == true){
-            return <li>Finished: {task}</li>
-        }
-        else {
-            return <li>Work on:{task} </li>
-        }
+//         if(isDone == true){
+//             return <li>Finished: {task}</li>
+//         }
+//         else {
+//             return <li>Work on:{task} </li>
+//         }
+// }
+
+// conditional rendering option:2
+// export default function Todo({task, isDone}) {
+//     if(isDone) {
+//         return <li>Finished: {task}</li>
+//     }
+//     return <li>Work on:{task} </li>
+// }
+
+// conditional rendering option: 3
+export default function Todo({task, isDone}){
+return (
+    <li>{isDone ? 'Finished' : 'Work on this'} : {task} </li>
+
+)
 }
